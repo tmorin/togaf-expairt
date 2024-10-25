c067d91d16e32da81af5994bef5e85e0d3c4044b
tmorin <thibault@morin.io>
2024-10-25 10:26:16 +0200

    feat: add more documents in the TOGAF Library Registry

--- src/main/java/io/morin/togafexpairt/core/TogafLibraryRegistry.java
+++ src/main/java/io/morin/togafexpairt/core/TogafLibraryRegistry.java
@@ -48,6 +48,69 @@ public enum TogafLibraryRegistry {
             URI.create("https://pubs.opengroup.org/togaf-standard/adm/chap12.html"),
             URI.create("https://pubs.opengroup.org/togaf-standard/adm/chap13.html")
         )
+    ),
+    /**
+     * TOGAF® Fundamental Content - ADM Techniques.
+     */
+    TOGAF_FUNDAMENTAL_ADM_TECHNIQUES(
+        "TOGAF® Fundamental Content - ADM Techniques",
+        List.of(
+            URI.create("https://pubs.opengroup.org/togaf-standard/adm-techniques/index.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/adm-techniques/chap01.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/adm-techniques/chap02.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/adm-techniques/chap03.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/adm-techniques/chap04.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/adm-techniques/chap05.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/adm-techniques/chap06.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/adm-techniques/chap07.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/adm-techniques/chap08.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/adm-techniques/chap09.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/adm-techniques/chap10.html")
+        )
+    ),
+    /**
+     * TOGAF® Fundamental Content - ADM Techniques.
+     */
+    TOGAF_FUNDAMENTAL_APPLYING_ADM(
+        "TOGAF® Fundamental Content - Applying the ADM",
+        List.of(
+            URI.create("https://pubs.opengroup.org/togaf-standard/applying-the-adm/index.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/applying-the-adm/chap01.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/applying-the-adm/chap02.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/applying-the-adm/chap03.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/applying-the-adm/chap04.html")
+        )
+    ),
+    /**
+     * TOGAF® Fundamental Content - ADM Techniques.
+     */
+    TOGAF_FUNDAMENTAL_ARCHITECTURE_CONTENT(
+        "TOGAF® Fundamental Content - Architecture Content",
+        List.of(
+            URI.create("https://pubs.opengroup.org/togaf-standard/architecture-content/index.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/architecture-content/chap01.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/architecture-content/chap02.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/architecture-content/chap03.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/architecture-content/chap04.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/architecture-content/chap05.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/architecture-content/chap06.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/architecture-content/chap07.html")
+        )
+    ),
+    /**
+     * TOGAF® Fundamental Content - ADM Techniques.
+     */
+    TOGAF_FUNDAMENTAL_CAPABILITY_GOVERNANCE(
+        "TOGAF® Fundamental Content - Enterprise Architecture Capability and Governance",
+        List.of(
+            URI.create("https://pubs.opengroup.org/togaf-standard/ea-capability-and-governance/index.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/ea-capability-and-governance/chap01.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/ea-capability-and-governance/chap02.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/ea-capability-and-governance/chap03.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/ea-capability-and-governance/chap04.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/ea-capability-and-governance/chap05.html"),
+            URI.create("https://pubs.opengroup.org/togaf-standard/ea-capability-and-governance/chap06.html")
+        )
     );
 
     /**
